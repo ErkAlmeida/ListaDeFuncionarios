9c63702f904791100af0b3970fff00f5d07ad764
Erick Almeida <erickprofissional@gmail.com>
2020-03-24 17:39:03 -0300

    Progeto Criado

---
+++ src/Programa.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+ * FazerumprogramaparalerumnúmerointeiroNedepoisosdados(id,nomeesalario)
+ * de Nfuncionários.Nãodevehaverrepetiçãodeid.
+ * Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário. 
+ * Paraisso,o programa deve ler um id eo valor X.
+ * Se o id informado não existir, mostrar uma mensagem e abortar a operação. 
+ * Ao final, mostrar a listagem atualizada dos funcionários, conformeexemplos.
+ * Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário 
+ * possa ser mudado livremente. Um salário só pode ser aumentado combase em uma operação de aumento por porcentagem dada
+ * 
+ */
+public class Programa {
+
+	public static void main(String[] args)
+	{
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+
+	}
+
+}
